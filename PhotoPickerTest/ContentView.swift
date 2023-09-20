60ebf3bcf106e957c54be65724ae6d741c69193d
An Byungwook <abw2619@icloud.com>
2023-09-20 17:17:14 +0900

    Initial Commit

---
+++ PhotoPickerTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoPickerTest
+//
+//  Created by 안병욱의 mac on 2023/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoPickerTest/PhotoPickerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerTestApp.swift
+//  PhotoPickerTest
+//
+//  Created by 안병욱의 mac on 2023/09/20.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
